a2df2561f980ab1d084633c57f7ca01080ffbe3f
Riari <3583774+Riari@users.noreply.github.com>
2024-05-14 23:37:03 +0100

    Start work on base mob type AI

---
+++ Source/ARPG/MobType/BaseMobTypeAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BaseMobTypeAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API ABaseMobTypeAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
